a864e8182c438f086e8b66572cf5e8f66d79ed83
Ujin <jenia17022@gmail.com>
2020-06-06 23:36:21 +0300

    refactor v1.0

---
+++ src/bpf/Fft.java
@@ -0,0 +1,115 @@
+package bpf;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class Fft {
+
+	public static double PI = 3.14159265358979323846264338327950288;
+	
+	public static void main(String args[]){
+		
+		Locale.setDefault(new Locale("ru", "RU"));
+		
+		Fft inst = new Fft();
+		
+	}
+
+	ArrayList<ComplexNumber> Fft( ArrayList<ComplexNumber> Points, int nstart, int nstep )
+	{
+		
+	    double tmp, angle, rw, iw;
+	    int n = 0, nn, i, j, k;
+	    int logN = 2;
+	    int N;
+	    
+	    if (nstep+nstart > Points.size()) nstep=Points.size()-nstart; 
+	    
+	    while (nstep > Math.pow(2, logN)) logN++;
+	    
+	    ArrayList<ComplexNumber> re = new ArrayList<ComplexNumber>((int)Math.pow(2, logN));
+
+	    for (i=0; i < (int)Math.pow(2, logN)+nstart; i++)
+	    {
+	    	re.add(i, new ComplexNumber(0,0));
+	        
+	    }
+	    
+	    for (i=nstart; i < nstep+nstart; i++)
+	    {
+	        re.get(i-nstart).real =  Points.get(i).real;
+	        re.get(i-nstart).img =  Points.get(i).img;
+	        
+	    }
+
+	    
+	    N = 1 << logN;
+	    
+	    for (i = N >> 1; i>0; i >>= 1) // выбираем числа согласно алгоритму прореживания по частоте
+	    {
+	    	
+	        for (j = 0; j < (N >> 1) / i; j++)
+	        {
+	        	
+	        	
+	            for (k = 0; k < i; k++)
+	            {
+	                // складываем значения
+	                nn = n + i;
+	                
+	                tmp = re.get(n).real + re.get(nn).real;
+	                re.get(nn).real = re.get(n).real - re.get(nn).real;
+	                re.get(n).real = tmp;
+	                
+	                tmp = re.get(n).img + re.get(nn).img;
+	                re.get(nn).img = re.get(n).img - re.get(nn).img;
+	                re.get(n).img = tmp;
+	                
+	                
+	                // умножаем на поворотные коэффициенты
+	                angle = PI / i * k;
+	                rw = Math.cos(angle);
+	                iw = Math.sin(angle);
+	                tmp = rw * re.get(nn).real - iw * re.get(nn).img;
+	                re.get(nn).img = rw * re.get(nn).img + iw * re.get(nn).real;
+	                re.get(nn).real = tmp;
+	                
+	                n++;
+	            }
+	            
+	            
+	            n = (n + i) & (N - 1); // обнуляет n в нужные моменты
+	            
+	        } 
+	    } 
+	    
+
+	    for (i=0; i < (int)Math.pow(2, logN); i++)
+	    {
+	    	ComplexNumber tm;
+	    	tm = re.get(i);
+	    	
+	    	re.get(i).img = re.get(getAddr(i, logN)).img;
+	    	re.get(i).real = re.get(getAddr(i, logN)).real;
+	    	
+	    	re.get(getAddr(i, logN)).img = tm.img;
+	    	re.get(getAddr(i, logN)).real = tm.real;	    	
+	    }
+	    
+	    
+	    return re;
+	} 
+	
+	int getAddr(int in, int logn)
+	{
+	    int res = 0;
+	    for (int i = 0; i < logn; ++i)
+	    {
+	        res <<= 1;
+	        res |= (in >> i) & 1;
+
+	    }
+
+	    return res;
+	}
+}
--- src/bpf/Sfft.java
+++ src/bpf/Sfft.java
@@ -13,6 +13,7 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import java.io.BufferedReader;
 import bpf.ComplexNumber;
+import bpf.Fft;
 import java.util.Arrays;
 import java.util.Locale;
 import com.google.common.primitives.Doubles;
@@ -25,7 +26,7 @@ public class Sfft {
 	double [][] powerful;
 	double [] time;
 	double [] freq;
-	double fs = 175.0;
+	double fs = 200.0;
 	double width = 0.1;
 	
 	public static void main(String args[]){
@@ -34,9 +35,11 @@ public class Sfft {
 		
 		Sfft inst = new Sfft();
 		
-		inst.readConfig(); 
+		inst.sigPrepare(); 
+		
 		inst.sfft();
 		inst.savePoints();
+		
 	}
 	
 	public void sigPrepare() {
@@ -46,17 +49,20 @@ public class Sfft {
 		int N = (int)((1-1/this.fs)/(1/this.fs)) + 1;
 		
 		for (k=0; k < N; k++) {
-			sPoints.add(new ComplexNumber(Math.sin(2 * PI * 10 * k / this.fs), 0));
-		}
-		
-		for (k=0; k < N; k++) {
 			sPoints.add(new ComplexNumber(Math.sin(2 * PI * 20 * k / this.fs), 0));
 		}
 		
 		for (k=0; k < N; k++) {
-			sPoints.add(new ComplexNumber(Math.sin(2 * PI * 30 * k / this.fs), 0));
+			sPoints.add(new ComplexNumber(Math.sin(2 * PI * 50 * k / this.fs), 0));
 		}
 		
+		for (k=0; k < N; k++) {
+			sPoints.add(new ComplexNumber(Math.sin(2 * PI * 80 * k / this.fs), 0));
+		}
+		
+		ArrayList<ComplexNumber> fftPoints = this.fft(sPoints, 0, N);
+		
+		System.out.println("");
 	}
 	
 	public void sfft() {
@@ -90,96 +96,6 @@ public class Sfft {
 		for (i = 0; i < window/2; i++) {
 			freq[i] = (double)i/window*fs;
 		}
-	}
-	
-	ArrayList<ComplexNumber> fft( ArrayList<ComplexNumber> Points, int nstart, int nstep )
-	{
-		
-	    double tmp, angle, rw, iw;
-	    int n = 0, nn, i, j, k;
-	    int logN = 2;
-	    int N;
-	    
-	    if (nstep+nstart > Points.size()) nstep=Points.size()-nstart; 
-	    
-	    while (nstep > Math.pow(2, logN)) logN++;
-	    
-	    ArrayList<ComplexNumber> re = new ArrayList<ComplexNumber>((int)Math.pow(2, logN));
-
-	    for (i=0; i < (int)Math.pow(2, logN)+nstart; i++)
-	    {
-	    	re.add(i, new ComplexNumber(0,0));
-	        
-	    }
-	    
-	    for (i=nstart; i < nstep+nstart; i++)
-	    {
-	        re.get(i-nstart).real =  Points.get(i).real;
-	        re.get(i-nstart).img =  Points.get(i).img;
-	        
-	    }
-
-	    
-	    N = 1 << logN;
-	    
-	    for (i = N >> 1; i>0; i >>= 1) // выбираем числа согласно алгоритму прореживания по частоте
-	    {
-	        for (j = 0; j < (N >> 1) / i; j++)
-	        {
-	            for (k = 0; k < i; k++)
-	            {
-	                // складываем значения
-	                nn = n + i;
-	                tmp = re.get(n).real + re.get(nn).real;
-	                re.get(nn).real = re.get(n).real - re.get(nn).real;
-	                re.get(n).real = tmp;
-	                
-	                tmp = re.get(n).img + re.get(nn).img;
-	                re.get(nn).img = re.get(n).img - re.get(nn).img;
-	                re.get(n).img = tmp;
-
-	                // умножаем на поворотные коэффициенты
-	                angle = PI / i * k;
-	                rw = Math.cos(angle);
-	                iw = Math.sin(angle);
-	                tmp = rw * re.get(nn).real - iw * re.get(nn).img;
-	                re.get(nn).img = rw * re.get(nn).img + iw * re.get(nn).real;
-	                re.get(nn).real = tmp;
-	                
-	                n++;
-	            }
-	            n = (n + i) & (N - 1); // обнуляет n в нужные моменты
-	        } 
-	    } 
-	    
-
-	    for (i=0; i < (int)Math.pow(2, logN); i++)
-	    {
-	    	ComplexNumber tm;
-	    	tm = re.get(i);
-	    	
-	    	re.get(i).img = re.get(getAddr(i, logN)).img;
-	    	re.get(i).real = re.get(getAddr(i, logN)).real;
-	    	
-	    	re.get(getAddr(i, logN)).img = tm.img;
-	    	re.get(getAddr(i, logN)).real = tm.real;	    	
-	    }
-	    
-	    
-	    return re;
-	} 
-	
-	int getAddr(int in, int logn)
-	{
-	    int res = 0;
-	    for (int i = 0; i < logn; ++i)
-	    {
-	        res <<= 1;
-	        res |= (in >> i) & 1;
-
-	    }
-
-	    return res;
 	}
 	
 	public void readConfig() {
@@ -227,7 +143,6 @@ public class Sfft {
 				}
 				points.write(x+'\n');
 			}
-			System.out.println(Locale.getDefault());
 			points.close();
 		 } catch (IOException e) {
 		    System.out.println("err");
